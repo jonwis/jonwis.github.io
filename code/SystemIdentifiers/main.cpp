2844846072278bb47e788585f04f8f10f59eb45c
Jon Wiswall <jonwis@ntdev.microsoft.com>
2024-01-24 11:15:55 -0800

    Samples for getting different kinds of system identifiers

---
+++ code/SystemIdentifiers/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <winrt/Windows.System.Profile.h>
+#include <winrt/Windows.Security.Cryptography.h>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::System::Profile;
+
+hstring to_base64(const Windows::Storage::Streams::IBuffer& buffer)
+{
+    return Windows::Security::Cryptography::CryptographicBuffer::EncodeToBase64String(buffer);
+}
+
+int main()
+{
+    init_apartment();
+    auto id = SystemIdentification::GetSystemIdForUser(nullptr);
+    printf("System ID: %ls\n", to_base64(id.Id()).c_str());
+    printf("Source: %d\n", id.Source());
+}
