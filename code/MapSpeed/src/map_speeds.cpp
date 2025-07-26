c0921b2686b6cd3b1550206044c6769867b7c93d
Jon Wiswall <jonwis@microsoft.com>
2025-07-26 10:55:54 -0700

    Make vector step equivalent to others

--- code/MapSpeed/src/map_speeds.cpp
+++ code/MapSpeed/src/map_speeds.cpp
@@ -51,6 +51,24 @@ template<typename T> std::vector<T> generateContent(uint64_t length)
     return randomContent;
 }
 
+struct timer {
+    std::chrono::high_resolution_clock::time_point start;
+    std::chrono::high_resolution_clock::time_point end;
+
+    timer() : start(std::chrono::high_resolution_clock::now()) {}
+
+    void stop()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
+    }
+
+    double duration()
+    {
+        return std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+    }
+};
+
 // Given an input length, generates a vector of random strings. Creates a map, an unordered_map, and a sorted vector of
 // the strings.
 template<typename T> void generateMaps(uint64_t length)
@@ -67,31 +85,35 @@ template<typename T> void generateMaps(uint64_t length)
     }
     std::sort(sortedVector.begin(), sortedVector.end());
 
+    std::cout.precision(5);
+    std::cout << std::fixed;
+    std::cout << std::setw(25) << "Length: " << length << "\n";
+
     // Shuffle the input random strings vector
     std::shuffle(randomContent.begin(), randomContent.end(), g_random);
 
     // Start a precise timer. Look up the strings in the map, and determine the time difference.
-    std::chrono::high_resolution_clock::time_point map_start = std::chrono::high_resolution_clock::now();
+    auto map_timer = timer();
     int sum = 0;
     for (auto& i : randomContent)
     {
         sum += map.find(i)->second;
     }
-    std::chrono::high_resolution_clock::time_point map_end = std::chrono::high_resolution_clock::now();
+    map_timer.stop();
     std::cout << "Sum: " << sum << "\n";
 
     // Start a precise timer. Look up the strings in the unordered_map, and determine the time difference.
-    std::chrono::high_resolution_clock::time_point unordered_map_start = std::chrono::high_resolution_clock::now();
+    auto unordered_map_timer = timer();
     sum = 0;
     for (auto& i : randomContent)
     {
         sum += unorderedMap.find(i)->second;
     }
-    std::chrono::high_resolution_clock::time_point unordered_map_end = std::chrono::high_resolution_clock::now();
+    unordered_map_timer.stop();
     std::cout << "Sum: " << sum << "\n";
 
     // Start a precise timer. Look up the strings in the sorted vector, and determine the time difference.
-    std::chrono::high_resolution_clock::time_point sorted_vector_start = std::chrono::high_resolution_clock::now();
+    auto sorted_vector_timer = timer();
     sum = 0;
     for (auto& i : randomContent)
     {
@@ -100,24 +122,21 @@ template<typename T> void generateMaps(uint64_t length)
         });
         if ((it != sortedVector.end()) && (it->first == i))
         {
-            sum += std::distance(sortedVector.begin(), it);
+            sum += it->second;
         }
     }
-    std::chrono::high_resolution_clock::time_point sorted_vector_end = std::chrono::high_resolution_clock::now();
+    sorted_vector_timer.stop();
     std::cout << "Sum: " << sum << "\n";
 
     // Output the time differences, and a "cycles per second" metric.
-    std::chrono::duration<double> map_time = std::chrono::duration_cast<std::chrono::duration<double>>(map_end - map_start);
-    std::chrono::duration<double> unordered_map_time = std::chrono::duration_cast<std::chrono::duration<double>>(unordered_map_end - unordered_map_start);
-    std::chrono::duration<double> sorted_vector_time = std::chrono::duration_cast<std::chrono::duration<double>>(sorted_vector_end - sorted_vector_start);
+    double map_time = map_timer.duration();
+    double unordered_map_time = unordered_map_timer.duration();
+    double sorted_vector_time = sorted_vector_timer.duration();
 
     // Set the double-formatting output to be 15 characters long, with 2 decimal places.
-    std::cout.precision(5);
-    std::cout << std::fixed;
-    std::cout << "Length: " << length << "\n";
-    std::cout << "Map time: " << map_time.count() << "s, " << ((double)length / map_time.count()) << " cycles per second\n";
-    std::cout << "Unordered map time: " << unordered_map_time.count() << "s, " << ((double)length / unordered_map_time.count()) << " cycles per second\n";
-    std::cout << "Sorted vector time: " << sorted_vector_time.count() << "s, " << ((double)length / sorted_vector_time.count()) << " cycles per second\n";
+    std::cout << std::setw(25) << "Map time: " << map_time << "s, " << std::setw(25) << ((double)length / map_time) << " cycles per second\n";
+    std::cout << std::setw(25) << "Sorted vector time: " << sorted_vector_time << "s, " << std::setw(25) << ((double)length / sorted_vector_time) << " cycles per second\n";
+    std::cout << std::setw(25) << "Unordered map time: " << unordered_map_time << "s, " << std::setw(25) << ((double)length / unordered_map_time) << " cycles per second\n";
 }
 
 template<typename T> void driver(std::string_view subject)
@@ -132,6 +151,7 @@ template<typename T> void driver(std::string_view subject)
 
 int main()
 {
+    std::cout << "In cycles per second, more is better; in time, less is better.\n";
     driver<std::string>("std::string");
     driver<uint32_t>("uint32_t");
 }
