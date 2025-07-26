234edcb8c926df5ba903539e16cd13cf8d1dec40
Jon Wiswall <jonwis@microsoft.com>
2025-07-26 09:38:39 -0700

    Sample of using API information for targeting

---
+++ code/VersionTargeting/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Windows.Foundation.Metadata;
+
+Console.WriteLine("Hello, World!");
+
+void SetInterval(Windows.Graphics.Capture.GraphicsCaptureSession session, int FrameRate)
+{
+    if (ApiInformation.IsPropertyPresent("Windows.Media.Capture.Frames.MediaFrameSource", "MinUpdateInterval"))
+    {
+        session.MinUpdateInterval = TimeSpan.FromMicroseconds(10000000 / FrameRate);
+    }
+}
+
