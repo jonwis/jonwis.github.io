019d7e2d88f44fd0efd779eb58fac310e7ea2244
Jon Wiswall <jonwis@microsoft.com>
2026-04-28 15:33:51 -0700

    More wacky coroutines

--- code/Coroutines/coroutines.cpp
+++ code/Coroutines/coroutines.cpp
@@ -7,6 +7,7 @@
 #include <wil/coroutine.h>
 #include <synchapi.h>
 #include <wil/coroutine.h>
+#include <winrt/Windows.Foundation.h>
 
 struct print_things
 {
@@ -55,8 +56,43 @@ wil::task<uint32_t> example_coroutine()
     co_return 42u;
 }
 
+winrt::Windows::Foundation::IAsyncAction test_async()
+{
+    printf("In start thread %lu\n", GetCurrentThreadId());
+    co_await winrt::resume_background();
+    printf("In background thread %lu\n", GetCurrentThreadId());
+}
+
+winrt::Windows::Foundation::IAsyncAction test_fromsta()
+{
+    printf("In STA thread %lu\n", GetCurrentThreadId());
+    co_await test_async();
+    printf("Back in STA thread %lu\n", GetCurrentThreadId());
+}
+
+void test()
+{
+    std::thread([] {
+        winrt::init_apartment(winrt::apartment_type::single_threaded);
+        auto q = test_fromsta();
+        wil::unique_event evt(wil::EventOptions::ManualReset);
+        q.Completed([&evt](auto&&...) {
+            evt.SetEvent();
+        });
+        HANDLE waitable = evt.get();
+        HRESULT hr = RPC_S_CALLPENDING;
+        while (hr == RPC_S_CALLPENDING)
+        {
+            DWORD which;
+            hr = CoWaitForMultipleHandles(COWAIT_DEFAULT, INFINITE, 1, &waitable, &which);
+        }        
+    }).join();
+}
+
 int main()
 {
+    test();
+
     printf("Starting coroutine from thread %lu\n", GetCurrentThreadId());
     auto task = example_coroutine();
     auto result = std::move(task).get();
